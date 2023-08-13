362c0c2add06203e7c507c2ef423a129efe115a8
Anastasiia Rakhmanova <rar.anastasiya@gmail.com>
2023-08-13 17:02:13 +0300

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,13 +5,26 @@ import org.junit.Assert;
 
 public class CashbackHackServiceTest {
     @org.junit.Test
-    public void cashbackHackServiceTest() {
+    public void cashbackHackServiceTest1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected =1000;
+        int expected = 0;
 
         Assert.assertEquals(actual, expected);
     }
-}
+
+    @org.junit.Test
+        public void cashbackHackServiceTest2() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 900;
+
+            int actual = service.remain(amount);
+            int expected = 100;
+
+            Assert.assertEquals(actual, expected);
+        }
+
+    }
+
